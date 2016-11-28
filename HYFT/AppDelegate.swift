d68f78b87a88477c07c6892635429ce678169ad1
Zac Kwan <weihao09@gmail.com>
2016-11-28 15:46:48 +0800

    Initial Commit

---
+++ HYFT/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HYFT
+//
+//  Created by Zac on 28/11/16.
+//  Copyright © 2016 zeta. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HYFT/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HYFT
+//
+//  Created by Zac on 28/11/16.
+//  Copyright © 2016 zeta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
